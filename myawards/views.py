667f45c67b689b415e1677691952d592d444d26a
poldiee <nessidave@gmail.com>
2022-06-12 01:09:27 +0300

    form addition for user registration into application

--- myawards/views.py
+++ myawards/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import SignupForm
 
 # Create your views here.
 def index(request):
@@ -6,4 +7,7 @@ def index(request):
 
 
 def signup(request):
-    return render(request, 'registration/signup.html')
+
+    form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form })
+
