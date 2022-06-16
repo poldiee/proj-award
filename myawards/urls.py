574d0f88d0f84aee878a4bc28bf11a48f7ad8937
poldiee <nessidave@gmail.com>
2022-06-17 00:58:07 +0300

    display more details of single image when user clicks on an image

--- myawards/urls.py
+++ myawards/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('profile/<username>/', views.profile, name='profile'),
+    path('project/<post>', views.project, name='project')
 ] 
