06b5ebd609e7964328832a74ae8402ba93adb48d
poldiee <nessidave@gmail.com>
2022-06-16 20:49:17 +0300

    creation of users endpoint

--- myawards/views.py
+++ myawards/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import SignupForm
+from rest_framework import viewsets
+from .models import Profile
+from .serializers import ProfileSerializer
 from django.contrib.auth import login, authenticate
 
 # Create your views here.
@@ -7,8 +10,12 @@ def index(request):
     return render(request, 'index.html')
 
 
-def signup(request):
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    print(queryset)
+    serializer_class = ProfileSerializer
 
+def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
@@ -25,4 +32,3 @@ def signup(request):
 
 def profile(request, username):
     return render(request, 'profile.html')
-
